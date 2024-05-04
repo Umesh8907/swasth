6cb33880ce7f6ffdb3d181508d7b263009dde22e
Umesh8907 <umesh.tandon.51@gmail.com>
2024-05-04 17:10:13 +0530

    swasth office

---
+++ app/_components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+const Header = () => {
+  return (
+    <div>
+        <Image src="/logo.svg" alt='logo'/>
+    </div>
+  )
+}
+
+export default Header
